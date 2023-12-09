dfa9e757b36299b456b54949e80cc053a125ef87
hiba el ouafi <elouafihiba1@gmail.com>
2023-12-09 12:19:24 +0000

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import {HomeComponent} from "./home/home.component";
 import {ProductComponent} from "./product/product.component";
 import {NewProductComponent} from "./new-product/new-product.component";
 import {EditProductComponent} from "./edit-product/edit-product.component";
+import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
+  {path:"login",component:LoginComponent,},
   {path:"home",component:HomeComponent,},
   {path:"products",component:ProductComponent,},
   {path:"newProduct",component:NewProductComponent,},
-  {path:"editProduct/:id",component:EditProductComponent,}
+  {path:"editProduct/:id",component:EditProductComponent,},
+  {path:"",redirectTo:"login",pathMatch:'full'}
 
 
 
